f55494675bea6d4da2c5b6343f0c5808f0ce03c1
Kartik Murali <muralikartik01@gmail.com>
2022-09-27 23:28:42 +0530

    v3

--- kartik_assignment2/app.py
+++ kartik_assignment2/app.py
@@ -7,6 +7,13 @@ app = Flask(__name__)
 def index():                                     #index function
     return render_template('index.html')
 
+@app.route('/login',methods=['GET','POST'])
+def login():                                     
+    return render_template('login.html')
+
+@app.route('/register',methods=['GET','POST'])
+def register():                                     
+    return render_template('register.html')
 
 
 if __name__=="__main__":
--- kartik_assignment2/venv/Scripts/activate.bat
+++ kartik_assignment2/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\ibm\venv
+set VIRTUAL_ENV=D:\ibm\IBM-Project-11356-1659321196\kartik_assignment2\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
